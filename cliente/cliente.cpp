22a9c9b9ec55646e46b96d80488771fc2152de41
Davi <daviknatume@gmail.com>
2026-09-17 16:56:28 -0300

    feat: commit inicial

---
+++ cliente/cliente.cpp
@@ -0,0 +1,22 @@
+//
+// CLIENTE DE CONTA BANCÁRIA SIMPLES
+//
+// Sistemas Distribuídos
+// Escola Politécnica -- PUCPR
+// (C) Prof. Luiz Lima Jr. (luiz.lima@pucpr.br)
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// 1. Inicializa ORB
+	// 2. Obtém referência para objeto distirbuído (da IOR)
+	// 3. Usa objeto (chama métodos)
+	// 4. Finalizações
+
+	return 0;
+}
---
+++ servidor/servidor.cpp
@@ -0,0 +1,25 @@
+//
+// SERVIDOR DE CONTA SIMPLES
+// Sistemas Distribuídos
+// Escola Politécnica -- PUCPR
+// (C) Prof. Luiz Lima Jr. (luiz.lima@pucpr.br)
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// 1. Inicia ORB
+	// 2. Ativa RootPOA
+	// 3. Instancia "servants"
+	// 4. Registra servos no POA, criando objetos distribuídos
+	// 5. Publica IOR
+	// 6. Aguarda requisições
+	// 7. Finalizações
+
+    return 0;
+}
